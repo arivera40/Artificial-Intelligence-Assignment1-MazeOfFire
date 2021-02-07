78ebdc17f839b82d39bdbe667f46e99c1eee4f39
Andy J Rivera <rivera.andy40@gmail.com>
2021-02-07 13:35:27 -0500

    Update Point.java

--- MazePackage/Point.java
+++ MazePackage/Point.java
@@ -6,11 +6,24 @@ public class Point {
     public Point parent;
     public int x;
     public int y;
+    public int stepsTaken;
+    public int heuristic;
 
     public Point(Point parent, int x, int y){
         this.parent = parent;
         this.x = x;
         this.y = y;
+        this.stepsTaken = 0;
+        this.heuristic = 0;
+
+    }
+
+    public Point(Point parent, int x, int y, int stepsTaken, int heuristic){
+        this.parent = parent;
+        this.x = x;
+        this.y = y;
+        this.stepsTaken = stepsTaken;
+        this.heuristic = heuristic;
     }
 
     //Compares two points to see if they are equal
@@ -32,4 +45,5 @@ public class Point {
         }
         return false;
     }
+
 }
